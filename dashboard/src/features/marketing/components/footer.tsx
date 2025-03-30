91c788dc05349c9af8f382fc406dfaa396b6a0a2
donatas <donatas.andros@gmail.com>
2025-03-30 13:34:34 +0300

    update footer

--- dashboard/src/features/marketing/components/footer.tsx
+++ dashboard/src/features/marketing/components/footer.tsx
@@ -1,22 +1,21 @@
-import { Link } from "@tanstack/react-router";
+import { FaDiscord, FaGithub, FaXTwitter } from "react-icons/fa6";
 
 export function Footer() {
   return (
     <footer className="bg-gray-50 px-4 py-12 md:px-8 dark:bg-gray-900">
       <div className="mx-auto flex max-w-[1280px] flex-col gap-4 md:flex-row md:items-center md:justify-between">
-        <Link to="/">
-          <img
-            src="https://cdn.zenpa.xyz/3Cg5.svg"
-            alt="Logo"
-            className="h-8 dark:hidden"
-          />
-          <img
-            src="https://cdn.zenpa.xyz/Cr3O.svg"
-            alt="Logo"
-            className="hidden h-8 dark:block"
-          />
-        </Link>
-        <p className="text-gray-500 dark:text-gray-400">
+        <div className="flex flex-1 items-center gap-x-6">
+          <a href="">
+            <FaDiscord className="size-6 fill-gray-400 hover:fill-gray-500 dark:fill-gray-600 dark:hover:fill-gray-500" />
+          </a>
+          <a href="">
+            <FaGithub className="size-6 fill-gray-400 hover:fill-gray-500 dark:fill-gray-600 dark:hover:fill-gray-500" />
+          </a>
+          <a href="">
+            <FaXTwitter className="size-6 fill-gray-400 hover:fill-gray-500 dark:fill-gray-600 dark:hover:fill-gray-500" />
+          </a>
+        </div>
+        <p className="flex-1 text-right text-gray-500 dark:text-gray-400">
           © 2025 Planora. All rights reserved.
         </p>
       </div>
