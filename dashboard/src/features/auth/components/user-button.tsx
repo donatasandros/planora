96d2707b105212ca773930ed024c04ff5bf40b72
donatas <donatas.andros@gmail.com>
2025-03-30 21:55:58 +0300

    reduce auth api calls

--- dashboard/src/features/auth/components/user-button.tsx
+++ dashboard/src/features/auth/components/user-button.tsx
@@ -1,4 +1,4 @@
-import { Link, useRouter } from "@tanstack/react-router";
+import { Link, useRouteContext, useRouter } from "@tanstack/react-router";
 import {
   LayoutGridIcon,
   LogOutIcon,
@@ -18,21 +18,18 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "~/components/ui/dropdown-menu";
-import { Skeleton } from "~/components/ui/skeleton";
 import { authClient } from "~/lib/auth-client";
 import { getInitials } from "~/utils/get-initials";
 
 export function UserButton() {
   const router = useRouter();
+  const { user } = useRouteContext({
+    from: "__root__",
+  });
 
-  const { data, isPending } = authClient.useSession();
   const { setTheme, theme } = useTheme();
 
-  if (isPending) {
-    return <Skeleton className="size-10 rounded-full bg-gray-200" />;
-  }
-
-  if (!data?.user) {
+  if (!user) {
     return null;
   }
 
@@ -50,34 +47,31 @@ export function UserButton() {
     <DropdownMenu>
       <DropdownMenuTrigger className="focus-visible:ring-brand-500 dark:focus-visible:ring-brand-400 size-10 rounded-full focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-white focus-visible:outline-none dark:focus-visible:ring-offset-gray-950">
         <Avatar>
-          <AvatarImage
-            src={data.user.image as string}
-            alt={`${data.user.name} avatar`}
-          />
-          <AvatarFallback>{getInitials(data.user.name)}</AvatarFallback>
+          <AvatarImage src={user.image as string} alt={`${user.name} avatar`} />
+          <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" sideOffset={8} className="w-[248px]">
         <div className="mb-1 flex items-center gap-x-3 border-b border-gray-200 px-4 py-3 dark:border-gray-800">
           <Avatar>
             <AvatarImage
-              src={data.user.image as string}
-              alt={`${data.user.name} avatar`}
+              src={user.image as string}
+              alt={`${user.name} avatar`}
             />
-            <AvatarFallback>{getInitials(data.user.name)}</AvatarFallback>
+            <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
           </Avatar>
           <div className="w-full overflow-hidden">
             <p
-              title={data.user.name}
+              title={user.name}
               className="truncate text-sm font-semibold text-gray-700 dark:text-gray-300"
             >
-              {data.user.name}
+              {user.name}
             </p>
             <p
-              title={data.user.email}
+              title={user.email}
               className="truncate text-sm text-gray-600 dark:text-gray-400"
             >
-              {data.user.email}
+              {user.email}
             </p>
           </div>
         </div>
