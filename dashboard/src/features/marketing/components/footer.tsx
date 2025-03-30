48cba23a8d2e70a4f14560cefcc2e64c7cd1edaa
donatas <donatas.andros@gmail.com>
2025-03-30 13:36:42 +0300

    fix mobile footer

--- dashboard/src/features/marketing/components/footer.tsx
+++ dashboard/src/features/marketing/components/footer.tsx
@@ -3,8 +3,8 @@ import { FaDiscord, FaGithub, FaXTwitter } from "react-icons/fa6";
 export function Footer() {
   return (
     <footer className="bg-gray-50 px-4 py-12 md:px-8 dark:bg-gray-900">
-      <div className="mx-auto flex max-w-[1280px] flex-col gap-4 md:flex-row md:items-center md:justify-between">
-        <div className="flex flex-1 items-center gap-x-6">
+      <div className="mx-auto flex max-w-[1280px] flex-col gap-6 md:flex-row md:items-center md:justify-between">
+        <div className="flex items-center gap-x-6">
           <a href="">
             <FaDiscord className="size-6 fill-gray-400 hover:fill-gray-500 dark:fill-gray-600 dark:hover:fill-gray-500" />
           </a>
@@ -15,7 +15,7 @@ export function Footer() {
             <FaXTwitter className="size-6 fill-gray-400 hover:fill-gray-500 dark:fill-gray-600 dark:hover:fill-gray-500" />
           </a>
         </div>
-        <p className="flex-1 text-right text-gray-500 dark:text-gray-400">
+        <p className="text-gray-500 dark:text-gray-400">
           © 2025 Planora. All rights reserved.
         </p>
       </div>
