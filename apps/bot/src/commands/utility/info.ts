310a2e7719593705fc07f2578996c1729321a555
Donatas <donatas@andros.lt>
2026-08-30 13:25:06 +0300

    feat(bot): add bot information command

---
+++ apps/bot/src/commands/utility/info.ts
@@ -0,0 +1,65 @@
+import {
+  type Client,
+  type CommandInteraction,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "discord.js"
+import { getSystemInfo } from "@/utils/system-info"
+import { formatTime } from "@/utils/time"
+
+export const data = new SlashCommandBuilder()
+  .setName("info")
+  .setDescription("View bot performance, statistics, and system information.")
+
+export async function execute(client: Client, interaction: CommandInteraction) {
+  await interaction.deferReply()
+
+  const reply = await interaction.fetchReply()
+
+  const systemInfo = await getSystemInfo()
+
+  const clientLatency = reply.createdTimestamp - interaction.createdTimestamp
+  const websocketLatency = client.ws.ping
+
+  const totalUsers = client.guilds.cache.reduce(
+    (acc, guild) => acc + guild.memberCount,
+    0
+  )
+
+  const embed = new EmbedBuilder()
+    .setColor(0x8762e4)
+    .setTitle("Bot Information")
+    .addFields(
+      {
+        name: "System",
+        value:
+          `• Node.js: \`${systemInfo.system.nodeVersion}\`\n` +
+          `• Platform: \`${systemInfo.system.platform}\``,
+      },
+      {
+        name: "Performance",
+        value:
+          `• Memory: \`${systemInfo.process.memoryUsage}/${systemInfo.system.totalMemory} MB\`\u200b\u200b\n` +
+          `• CPU Usage: \`${systemInfo.process.cpuUsage}%\`\n` +
+          `• Uptime: \`${formatTime(client.uptime ?? 0)}\`\n`,
+      },
+      {
+        name: "Latency",
+        value:
+          `• Client Latency: \`${clientLatency} ms\`\n` +
+          `• WebSocket Latency: \`${websocketLatency} ms\`\n\n`,
+      },
+      {
+        name: "Statistics",
+        value:
+          `• Servers: \`${client.guilds.cache.size.toLocaleString()}\`\n` +
+          `• Users: \`${totalUsers}\`\n\n`,
+      },
+      {
+        name: "Links",
+        value: `[• Dashboard](https://TODO.com)\n[• Support Server](https://TODO.com)`,
+      }
+    )
+
+  await interaction.editReply({ embeds: [embed] })
+}
